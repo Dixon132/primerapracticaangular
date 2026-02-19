8fcae9e6b55e73b80dbac11cfb228a06d94e3b60
Diego <diegoalextorrez@gmail.com>
2026-02-18 20:25:31 -0400

    primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { EditorFiltro } from './editor-filtro/editor-filtro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [EditorFiltro],
+  template: `<app-editor-filtro></app-editor-filtro>`,
 })
-export class App {
-  protected readonly title = signal('primerapracticaangular');
-}
+export class App { }
