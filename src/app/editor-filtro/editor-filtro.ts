996f365b78681b1846ca1836a5c596f0c5af0491
Diego <diegoalextorrez@gmail.com>
2026-02-18 20:48:45 -0400

    practicaangular segundo commit casi terminado

--- src/app/editor-filtro/editor-filtro.ts
+++ src/app/editor-filtro/editor-filtro.ts
@@ -10,15 +10,35 @@ export class EditorFiltro {
   brillo = signal(100)
   contraste = signal(100)
   blur = signal(0)
+  modoVintage = signal(false);
   filtroScss = computed(() => {
+    const base = `
+    brightness(${this.brillo()}%)
+    contrast(${this.contraste()}%)
+    blur(${this.blur()}px)
+  `;
 
-    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
-  })
+    if (this.modoVintage()) {
+      return base + `
+      sepia(60%)
+      saturate(90%)
+      grayscale(40%)
+    `;
+    }
+
+    return base;
+  });
+
+
   actualizar(prop: string, evento: Event) {
     const valor = (evento.target as HTMLInputElement).value
     if (prop === 'brillo') this.brillo.set(+valor);
     if (prop === 'contraste') this.contraste.set(+valor);
     if (prop === 'blur') this.blur.set(+valor);
-
   }
+
+  toggleSepia() {
+    this.modoVintage.update(v => !v);
+  }
+
 }
